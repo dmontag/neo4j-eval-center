314e77c76aad2a2e505722c22db47a9e66f412c2
David Montag <david.montag@neotechnology.com>
2013-07-20 06:26:27 +0200

    Added fully generated queries for social

--- src/console/generate.sh
+++ src/console/generate.sh
@@ -1,9 +1,26 @@
 #!/bin/sh
 
-for file in *.txt; do
-    curl -d @$file http://console.neo4j.org/console/cypher \
+rm *.compiled *.viz
+
+for base in *.txt; do
+
+    curl -d @$base http://console.neo4j.org/console/cypher \
     | ruby -rjson -e 'puts JSON.dump(JSON.parse(STDIN.read)["visualization"])' \
-    > $file.viz
+    > $base.viz
+
+    for query in `ls $base.frag*`; do
+
+        cat $base > $query.compiled
+        echo " WITH count(*) AS __ " >> $query.compiled
+        cat $query >> $query.compiled
+
+        curl -d @$query.compiled http://console.neo4j.org/console/cypher \
+        | ruby -rjson -e 'puts JSON.dump(JSON.parse(STDIN.read)["visualization"])' \
+        > $query.viz
+    done
 done
 
+rm *.compiled
+rm ../../viz/*
 mv *.viz ../../viz/
+cp *.frag* ../../viz/
